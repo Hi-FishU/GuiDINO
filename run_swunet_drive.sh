32ad42669d267dcf78f85c7b1cc3aab7a3b6c4ae
Zhuonan Liang <zhuonan@sydney.edu.au>
2026-03-01 23:55:09 +1100

    Publish update

--- run_swunet_drive.sh
+++
@@ -1,5 +0,0 @@
-cd /home/zhuonan/code/MedToken
-python train.py \
-  --drive-root data_source/drive \
-  --loss dc_bce \
-  --max-epochs 2000
--- run_unet_Kvasir.sh
+++
@@ -1,8 +0,0 @@
-cd /home/zhuonan/code/MedToken
-python train.py \
-  --model unet \
-  --run-model unet-kvasir \
-  --kvasir-root data_source/Kvasir \
-  --loss dc_bce \
-  --image-size 512 \
-  --no-train-epoch-eval
